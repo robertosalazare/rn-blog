bb8a9616f6ae65b5ede35fd5a22805a529519425
robertosalazare <jrse95@gmail.com>
2018-12-16 15:49:22 -0700

    all files added

---
+++ app.js
@@ -0,0 +1,26 @@
+let express = require("express");
+let app = express();
+let path = __dirname + '/views';
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+  console.log("/" + req.method);
+  next();
+});
+
+app.get("/",(req,res) => {
+  res.sendFile(path + "/index.html");
+});
+
+app.get("/age-gender",(req,res) => {
+  res.sendFile(path + "/age-gender.html");
+});
+
+app.get("/cards-generation",(req,res) => {
+  res.sendFile(path + "/cards-generation.html");
+});
+
+app.listen(3000, () => {
+  console.log("Live at Port 3000");
+});
